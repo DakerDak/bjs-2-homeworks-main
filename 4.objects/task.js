036dc4dceeacbee5ed44333796e693ed40486bd5
Trevor Quinn <dilam-rt@yandex.ru>
2023-03-18 13:07:47 +0300

    ferst commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.subject;
+  this.marks = new Array();
+  this.excluded;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark)
+{
+    this.marks.push(mark);  
+}
+
+
+Student.prototype.addMarks = function (...marks)
+{
+for(mark of marks)
+  this.marks.push(mark);
+}
+
+
+Student.prototype.getAverage = function ()
+{
+var sum = 0;
+this.marks.forEach(element => {
+    sum += element;
+})
+
+return (sum / this.marks.length);
+}
+
+
+Student.prototype.exclude = function (reason)
+{
+delete this.subject;
+delete this.marks;
+this.excluded = reason;
+}
---
+++ 5.classes/task.js
@@ -0,0 +1,120 @@
+class PrintEditionItem
+{
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this._state = 100;
+    this.type  = null;
+}
+
+  get state() {
+    return this._state;
+  }
+
+  set state(value) {
+    if (value >= 100) {this._state= 100;}
+    else if(value <= 0) {this._state = 0;}
+    else {this._state = value;}
+  }
+}
+
+
+PrintEditionItem.prototype.fix = function () {
+  this.state = this.state * 1.5;
+}
+
+
+class Magazine extends PrintEditionItem {
+  type = "magazine";
+}
+
+class Book extends PrintEditionItem {
+  type = "book";
+  
+  constructor(author, name, releaseDate, pagesCount) 
+  {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+  }
+}
+
+class NovelBook extends Book {
+  type = "novel";
+}
+
+class FantasticBook extends Book {
+  type = "fantastic";
+}
+
+class DetectiveBook extends Book {
+  type = "detective";
+}
+
+
+// Задача 2. Библиотека
+
+class Library 
+{
+  constructor(name) {
+    this.name = name;
+    this.books = new Array();
+  }
+
+  addBook(book)
+  {
+    if(book.state >= 30)
+      this.books.push(book);
+  }
+
+  // Найти книгу
+  findBookBy(type, value)
+  {
+    var book;
+    if(type == "name")
+    {
+      this.books.forEach(element => {              
+            if(element.name == value) {
+                book = element;
+            }
+      });
+    }
+    else if(type == "releaseDate")
+    {
+      this.books.forEach(element => {              
+            if(element.releaseDate == value) {
+                book = element;
+            }
+      });
+    }
+    else if(type == "pagesCount")
+    {
+      this.books.forEach(element => {              
+            if(element.pagesCount == value) {
+                book = element;
+            }
+      });
+    }
+
+    if(!book) book = null; // если не нашли книгу = null
+    return book;
+  }
+
+  // Выдать книгу
+  giveBookByName(bookName)
+  {
+    var index;
+    var book;
+    for(var i = 0; i < this.books.length; i++)
+      if(this.books[i].name == bookName) 
+      {
+        index = i;
+        book = this.books[i];
+      }
+    this.books.splice(index);
+    
+    if(!book) book = null; // если не нашли книгу = null
+    return book;
+  }
+}
+
